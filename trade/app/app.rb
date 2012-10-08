87748d01a942c9339849937c074a809bf3b4c477
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2012-10-08 13:19:46 +0200

    more refactoring --Thanks Misch!
    	modified:   trade/app/app.rb
    	deleted:    trade/app/init.rb

--- trade/app/app.rb
+++ trade/app/app.rb
@@ -13,13 +13,15 @@ require_relative('controllers/signup')
 require_relative('../../trade/app/models/module/item')
 require_relative('../../trade/app/models/module/user')
 
+include Models
+include Controllers
 
 class App < Sinatra::Base
 
-use Controllers::Authentication
-use Controllers::Sites
-use Controllers::Creator
-use Controllers::Signup
+use Authentication
+use Sites
+use Creator
+use Signup
 
 enable :sessions
 set :views, relative('app/views')
@@ -27,7 +29,7 @@ set :public_folder, 'public'
 
 
 configure :development do
-  userA = Models::User.created( "userA", "passwordA" )
+  userA = User.created( "userA", "passwordA" )
   userA.save
   aa = userA.create_item("UserA_ItemA", 10)
   ab = userA.create_item("UserA_ItemB", 50)
@@ -35,7 +37,7 @@ configure :development do
   ac = userA.create_item("UserA_ItemC", 120)
   ac.active = true
 
-  userB = Models::User.created( "userB", "passwordB" )
+  userB = User.created( "userB", "passwordB" )
   userB.save
   ba = userB.create_item("UserB_ItemA", 10)
   ba.active = true
@@ -43,7 +45,7 @@ configure :development do
   bb.active = true
   bc = userB.create_item("UserB_ItemC", 120)
 
-  userC = Models::User.created( "userC", "passwordC" )
+  userC = User.created( "userC", "passwordC" )
   userC.save
   ca = userC.create_item("UserC_ItemA", 10)
   ca.active = true
@@ -51,7 +53,7 @@ configure :development do
   cc = userC.create_item("UserC_ItemC", 120)
   cc.active = true
 
-  ese = Models::User.created( "ese", "ese" )
+  ese = User.created( "ese", "ese" )
   ese.save
 
   end
