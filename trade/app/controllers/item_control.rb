b6126b4410bb02c9e20d5eb9eb0509af152a32f5
Remo Burn <remo.burn@students.unibe.ch>
2012-10-12 13:18:00 +0200

    New Controller Classes

---
+++ trade/app/controllers/item_control.rb
@@ -0,0 +1,178 @@
+def relative(path)
+  File.join(File.expand_path(File.dirname(__FILE__)), path)
+end
+require 'rubygems'
+require 'require_relative'
+require 'sinatra/base'
+require 'haml'
+require 'sinatra/content_for'
+require_relative('../models/module/user')
+require_relative('../models/module/item')
+
+include Models
+
+module Controllers
+  class ItemControl < Sinatra::Base
+    set :views, relative('../../app/views')
+    helpers Sinatra::ContentFor
+
+    before do
+      @user = User.get_user(session[:username])
+    end
+
+    get '/home/active' do
+      redirect '/index' unless session[:username]
+      haml :home_all_items, :locals => {:user => @user, :page_name => "Your items", :error => nil}
+    end
+
+    get '/home/inactive' do
+      redirect '/index' unless session[:username]
+      haml :home_inactive, :locals => {:inactive_items => @user.list_items_inactive, :page_name => "Inactive items", :error => nil}
+    end
+
+    get '/home/new' do
+      redirect '/index' unless session[:username]
+      haml :home_new, :locals =>{:action => "create", :name => "", :price => "", :description =>"", :button => "Create", :page_name => "New Item", :error => nil}
+    end
+
+    get '/home/edit_item/:itemid' do
+      redirect '/index' unless session[:username]
+      if Item.get_item(params[:itemid]).is_owner?(@user.name)
+
+        item = Item.get_item(params[:itemid])
+        item_name = item.name
+        price = item.price
+        description = item.description
+        unless Item.valid_price?(String(price))
+          redirect "/home/edit_item/#{params[:itemid]}/not_a_number"
+        end
+
+        # MW: To do: Get the right params.
+        haml :home_new, :locals => {:action => "edit_item/#{params[:itemid]}", :name => item_name, :price => price, :description => description, :button => "Save changes", :page_name => "Edit Item", :error => nil}
+      else
+        redirect "/"
+      end
+    end
+
+    get '/home/edit_item/:itemid/:error_msg' do
+      redirect '/index' unless session[:username]
+      if Item.get_item(params[:itemid]).is_owner?(@user.name)
+        item = Item.get_item(params[:itemid])
+        item_name = item.name
+        price = item.price
+        description = item.description
+
+        case params[:error_msg]
+          when "not_a_number"
+            haml :home_new, :locals => {:action => "edit_item/#{params[:itemid]}", :name => item_name, :price => price, :description => description, :button => "Edit", :page_name => "Edit Item", :error => "Your price is not a valid number!"}
+          when "no_name"
+            haml :home_new, :locals => {:action => "edit_item/#{params[:itemid]}", :name => item_name, :price => price, :description => description, :button => "Edit", :page_name => "Edit Item", :error => "You have to choose a name for your item!"}
+        end
+      else
+        redirect "/"
+      end
+    end
+
+    get '/items' do
+      redirect '/index' unless session[:username]
+      haml :items, :locals => {:all_items => Item.get_all(@user.name), :page_name => "Items", :error => nil }
+    end
+
+    get '/items/:error_msg' do
+      redirect '/index' unless session[:username]
+      case params[:error_msg]
+        when "not_enough_credits"
+          haml :items, :locals => {:all_items => Item.get_all(@user.name), :page_name => "Items", :error => "Not enough credits!" }
+        when "out_of_sync"
+          haml :items, :locals => {:all_items => Item.get_all(@user.name), :page_name => "Items", :error => "Item has been edited while you tried to buy it!" }
+      end
+    end
+
+    post '/create' do
+      redirect '/index' unless session[:username]
+      user = session[:username]
+      price = params[:price]
+
+      unless Item.valid_price?(price)
+        redirect "create/not_a_number"
+      end
+      #error handling for empty names or whitespaces (strip removes all kind of whitespaces, but not the first space)
+      unless params[:name].strip.delete(' ')!=""
+        redirect '/create/no_name'
+      end
+      User.get_user(user).create_item(params[:name], Integer(params[:price]), params[:description])
+      # MW: maybe "User.by_name" might be somewhat more understandable
+      redirect "/home/active"
+    end
+
+    get '/create/:error_msg' do
+      redirect '/index' unless session[:username]
+      case params[:error_msg]
+        when "not_a_number"
+          haml :home_new, :locals =>{:action => "create", :name => "", :price => "", :description =>"", :button => "Create", :page_name => "New Item", :error => "Your price is not a valid number!"}
+        when "no_name"
+          haml :home_new, :locals =>{:action => "create", :name => "", :price => "", :description =>"", :button => "Create", :page_name => "New Item", :error => "You have to choose a name for your item!"}
+      end
+    end
+
+    post '/edit_item/:itemid' do
+      redirect '/index' unless session[:username]
+      #error handling for empty names or whitespaces (strip removes all kind of whitespaces, but not the first space)
+      unless params[:name].strip.delete(' ')!=""
+        redirect "/home/edit_item/#{params[:itemid]}/no_name"
+      end
+      item = Item.get_item(params[:itemid])
+      price = params[:price]
+      unless Item.valid_price?(String(price))
+        redirect "/home/edit_item/#{params[:itemid]}/not_a_number"
+
+      end
+      item.edit(params[:name],params[:price],params[:description])
+      redirect "/home/active"
+    end
+
+    post '/changestate/:id/setactive' do
+      redirect '/index' unless session[:username]
+      id = params[:id]
+      Item.get_item(id).active = true
+      redirect "/home/active"
+    end
+
+    post '/changestate/:id/setinactive' do
+      redirect '/index' unless session[:username]
+      id = params[:id]
+      Item.get_item(id).active = false
+      redirect "/home/active"
+    end
+
+    post '/buy/:id/:timestamp' do
+      redirect '/index' unless session[:username]
+      id = params[:id]
+      item = Item.get_item(id)
+      old_user = item.owner
+      user = session[:username]
+      new_user = User.get_user(user)
+      puts params[:timestamp]
+      puts "item:"
+      puts item.timestamp
+      if (Integer(params[:timestamp])-item.timestamp)!=0
+        if(back.include? '/out_of_sync')
+          redirect '#back'
+        else
+          redirect "#{back}/out_of_sync"
+        end
+      end
+      if new_user.buy_new_item?(item)
+        old_user.remove_item(item)
+      else
+        if back.include? '/not_enough_credits'
+          redirect "#{back}"
+        else
+          redirect "#{back}/not_enough_credits"
+        end
+      end
+      redirect "/home/active"
+    end
+
+  end
+end
---
+++ trade/app/controllers/user_control.rb
@@ -0,0 +1,125 @@
+def relative(path)
+  File.join(File.expand_path(File.dirname(__FILE__)), path)
+end
+require 'rubygems'
+require 'require_relative'
+require 'sinatra/base'
+require 'haml'
+require 'sinatra/content_for'
+require_relative('../models/module/user')
+require_relative('../models/module/item')
+
+include Models
+
+module Controllers
+  class UserControl < Sinatra::Base
+    set :views, relative('../../app/views')
+    set :public_folder, relative('../public')
+    helpers Sinatra::ContentFor
+
+    before do
+      @user = User.get_user(session[:username])
+    end
+
+    get '/logout' do
+      redirect '/index' unless session[:username]
+      haml :logout, :locals => {:page_name => "Logout", :error => nil}
+    end
+
+    get '/home' do
+      redirect '/index' unless session[:username]
+      haml :home, :locals => {:page_name => "Home", :error => nil}
+    end
+
+    get '/users' do
+      redirect '/index' unless session[:username]
+      haml :users, :locals => {:all_users => User.get_all(""), :page_name => "Users", :error => nil}
+    end
+
+    get '/users/:id' do
+      redirect '/index' unless session[:username]
+      user = params[:id]
+      if user == @user.name
+        redirect "/profile"
+      else
+        haml :users_id, :locals => {:active_items => User.get_user(user).list_items, :page_name => "User #{user}", :user => User.get_user(user), :error => nil}
+      end
+    end
+
+    get '/users/:id/:error_msg' do
+      redirect '/index' unless session[:username]
+      user = params[:id]
+      case params[:error_msg]
+        when "not_enough_credits"
+          haml :users_id, :locals => {:active_items => User.get_user(user).list_items, :user => User.get_user(user), :page_name => "User #{user}", :error => "Not enough credits!"}
+        when "out_of_sync"
+          haml :users_id, :locals => {:active_items => User.get_user(user).list_items, :user => User.get_user(user), :page_name => "User #{user}", :error => "Item has been edited while you tried to buy it!" }
+      end
+    end
+
+    post "/unauthenticate" do
+      redirect '/index' unless session[:username]
+      session[:username] = nil
+      #session['auth'] = false
+      redirect "/"
+    end
+
+    get '/profile' do
+      redirect '/index' unless session[:username]
+      haml :profile, :locals => {:user => @user, :description => @user.description, :page_name => "Your profile", :error => nil}
+    end
+
+    get "/profile/:error_msg" do
+      redirect '/index' unless session[:username]
+      if not session[:username].nil?
+        case params[:error_msg]
+          when "false_pw"
+            haml :profile, :locals => {:page_name => "Your profile", :user => User.get_user(session[:username]), :error => "You entered an incorrect password"}
+          when "mismatch"
+            haml :profile, :locals => {:page_name => "Your profile", :user => User.get_user(session[:username]), :error => "The new password and the check do not match"}
+          when "unsafe"
+            haml :profile, :locals => {:page_name => "Your profile", :user => User.get_user(session[:username]), :error => "Your password is unsafe"}
+        end
+      else
+        redirect "/"
+      end
+
+    end
+
+    post "/change_description" do
+      redirect '/index' unless session[:username]
+      viewer = User.get_user(session[:username])
+      to_insert = params[:description]
+      list = to_insert.split("\n")
+      viewer.description = list
+      redirect "/profile"
+    end
+
+    post "/change_password" do
+      redirect '/index' unless session[:username]
+      password_check = PasswordCheck.created
+      viewer = User.get_user(session[:username])
+      redirect "/profile/false_pw" if !(viewer.check_password(params[:password_old]))
+      redirect "/profile/mismatch" if params[:password_new]!=params[:password_check]
+      redirect "/profile/unsafe"   if !password_check.safe?(params[:password_new])
+      viewer.change_password(params[:password_new])
+      redirect "/"
+    end
+
+    get '/delete_link' do
+      redirect '/index' unless session[:username]
+      haml :delete_user, :locals =>{:page_name => "Delete Your Account", :error => nil}
+    end
+
+
+    get '/delete_account' do
+      #delete user
+      user = session[:username]
+      User.get_user(user).delete
+      #close session
+      session[:username] = nil
+      redirect '/index' unless session[:username]
+      haml :logout, :locals => {:page_name => "Logout", :error => nil}
+    end
+  end
+end
