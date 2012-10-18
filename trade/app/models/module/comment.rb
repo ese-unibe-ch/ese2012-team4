7bf090a646748c620380c708ac0487bd1d5302e4
Michele Wyss <michele_wyss@students.unibe.ch>
2012-10-18 16:47:04 +0200

    added new attribute "text" to comment. 	modified:   trade/app/models/module/comment.rb

--- trade/app/models/module/comment.rb
+++ trade/app/models/module/comment.rb
@@ -1,17 +1,20 @@
-class Comment
- # A Comment has an id (identifier), an author and there's an item on which the comment corresponds.
- # A Comment may be the answer on a previous comment, but does not have to.
+module Models
 
-  attr_accessor :id, :author, :correspondent_item , :previous_comment
-  @@count = 0
+  class Comment
+    # A Comment has an id (identifier), an author and there's an item on which the comment corresponds.
+    # A Comment may be the answer on a previous comment, but does not have to.
+    # Of course, a comment also has a text.
 
-  def self.created(author, correspondent_item, previous_comment = nil)
-    comment = self.new
-    comment.id = @@count+=1
-    comment.author = author
-    comment.correspondent_item = correspondent_item
-    comment.previous_comment = previous_comment
+    attr_accessor :id, :author, :correspondent_item , :previous_comment, :text
+    @@count = 0
+
+    def self.created(author, correspondent_item, text, previous_comment = nil)
+      comment = self.new
+      comment.id = @@count+=1
+      comment.author = author
+      comment.correspondent_item = correspondent_item
+      comment.previous_comment = previous_comment
+      comment.text = text
+    end
   end
-
-
 end
