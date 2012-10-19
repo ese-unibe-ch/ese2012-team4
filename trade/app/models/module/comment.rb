187cf029a4ee22784f9994f60c7adde9e9ec3a3c
Michele Wyss <michele_wyss@students.unibe.ch>
2012-10-19 13:01:39 +0200

    2 levels of comments 	modified:   trade/app/models/module/comment.rb

--- trade/app/models/module/comment.rb
+++ trade/app/models/module/comment.rb
@@ -4,9 +4,13 @@ module Models
     # A Comment has an id (identifier), an author and there's an item on which the comment corresponds.
     # A Comment may be the answer on a previous comment, but does not have to.
     # Of course, a comment also has a text.
+    # There will be 2 levels of Comments:
+    #   - head comments which correspond to an item and do not have a previous comment
+    #   - sub comments  which correspond to an item and a head comment
 
-    attr_accessor :id, :author, :correspondent_item , :previous_comment, :text
+    attr_accessor :id, :author, :correspondent_item, :previous_comment, :text, :sub_comments
     @@count = 0
+    @@comments = []
 
     def self.created(author, correspondent_item, text, previous_comment = nil)
       comment = self.new
@@ -16,5 +20,17 @@ module Models
       comment.previous_comment = previous_comment
       comment.text = text
     end
+  end
+
+  def self.list_comments(item)
+    @@comments.select{|comment| comment.correspondent_item.eql?(item)}
+  end
+
+  def save
+    @@comments.push(self)
+  end
+
+  def is_head_comment?
+    previous_comment.eql?(nil)
   end
 end
