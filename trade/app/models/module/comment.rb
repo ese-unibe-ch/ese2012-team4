5b5684d27520e38489d92b2bcaf1728400ce9410
Michele Wyss <michele_wyss@students.unibe.ch>
2012-10-19 17:53:12 +0200

    added a test class, preliminary completed contructor 	modified:   trade/app/models/module/comment.rb 	new file:   trade/test/comment_test.rb

--- trade/app/models/module/comment.rb
+++ trade/app/models/module/comment.rb
@@ -8,17 +8,19 @@ module Models
     #   - head comments which correspond to an item and do not have a previous comment
     #   - sub comments  which correspond to an item and a head comment
 
-    attr_accessor :id, :author, :correspondent_item, :previous_comment, :text, :sub_comments
-    @@count = 0
-    @@comments = []
+    attr_accessor :id, :author, :correspondent_item, :previous_comment, :text
+
+    @@comment_count = 0
+    @@comments = {}
 
     def self.created(author, correspondent_item, text, previous_comment = nil)
       comment = self.new
-      comment.id = @@count+=1
+      comment.id = @@comment_count + 1
       comment.author = author
       comment.correspondent_item = correspondent_item
       comment.previous_comment = previous_comment
       comment.text = text
+      comment
     end
   end
 
@@ -27,7 +29,7 @@ module Models
   end
 
   def save
-    @@comments.push(self)
+  # TODO
   end
 
   def is_head_comment?
