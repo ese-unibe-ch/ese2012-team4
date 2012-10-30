fe9143d9370de00f6f218e91877f3fddb5856d67
Simon Hirsbrunner <simon.a.hirsbrunner@gmail.com>
2012-10-30 11:35:01 +0100

    Added Pix for 4 test users
    Some minor changes to the Stories :)
    Use the "template" in app.rb to create your other users

--- trade/app/app.rb
+++ trade/app/app.rb
@@ -29,22 +29,28 @@ class App < Sinatra::Base
   set :public_folder, relative('public')
 
   configure :development do
-    userA = User.created( "Bad Simu", "password1", "e_mail_A@preset.com" )
+    userA = User.created( "Evil Simu", "password1", "e_mail_A@preset.com", "Music by Nino Rota" )
     userA.save
+    userA.image = FileUtils::pwd+"/public/images/user_pix/shirsbrunner2.jpeg"
     aa = userA.create_item("Half-empty hair wax", 15, 1)
     ab = userA.create_item("Vespa Primavera ET3", 250, 1)
     ab.description = "Rusty, but a true classic"
     ab.active = true
-    ac = userA.create_item("Beretta Model 38/42", 120, 1)
-    ac.description = "Only used in one murder"
+    ac = userA.create_item("Beretta Model 38/42", 60, 1)
+    ac.description = "Cheap, but only used once in family-business"
     ac.active = true
+
+    for i in 0..10
+     userA.add_rating(rand(5))
+    end
 
     co = ac.comment(userA, "Great Item! Please buy it!")
 
 
 
-    userB = User.created( "Bad Giänu", "password1", "e_mail_B@preset.com" )
+    userB = User.created( "Evil Giänu", "password1", "e_mail_B@preset.com" )
     userB.save
+    userB.image = FileUtils::pwd+"/public/images/user_pix/gl2.jpg"
     ba = userB.create_item("Black leather suitcase", 10, 2)
     ba.active = true
     ba.description = "Crappy, but stylish"
@@ -53,9 +59,13 @@ class App < Sinatra::Base
     bb.description = "Too lazy to wash, I'm buying new ones"
     co.answer(userB, "It doesn't seems that great. That model is quite loud, can't you go any lower than that?")
 
+    for i in 0..10
+      userB.add_rating(rand(5))
+    end
 
-    userC = User.created( "Bad lüku", "password1", "e_mail_C@preset.com" )
+    userC = User.created( "Evil Lüku", "password1", "e_mail_C@preset.com" )
     userC.save
+    userC.image = FileUtils::pwd+"/public/images/user_pix/ld_baaad.jpg"
     ca = userC.create_item("Rusty saber", 70, 1)
     ca.description="YAAAAAAAAAAAAAAARRRRRRRRRRRRR!!!"
     ca.active = true
@@ -66,8 +76,18 @@ class App < Sinatra::Base
     cc.active = true
     co.answer(userC, "You're right, those things will scare my rum off my ship!")
 
-    ese = User.created( "ese", "ese", "ese@preset.com" )
+    for i in 0..10
+      userC.add_rating(rand(5))
+    end
+
+    ese = User.created( "ese", "ese", "ese@preset.com", "fancy code-snippets" )
     ese.save
+    ese.image = FileUtils::pwd+"/public/images/user_pix/ese.jpeg"
+
+    for i in 0..10
+      ese.add_rating(4)
+    end
+
 
     end
 
