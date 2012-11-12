7b285809b6f4cc00060dfdf704351fdef6611fb7
shaechler <babemusicstar@hotmail.com>
2012-11-12 15:16:17 +0100

    added curent winner

--- trade/app/models/module/auction.rb
+++ trade/app/models/module/auction.rb
@@ -1,6 +1,6 @@
 module Models
   class Auction
-    attr_accessor :item, :owner, :increment, :min_prize, :end_time, :bids
+    attr_accessor :item, :owner, :increment, :min_prize, :end_time, :bids, :current_winner
 
 
 
@@ -10,14 +10,24 @@ module Models
       self.increment= increment
       self.min_prize= min_prize
       self.end_time= end_time
-      @bids = Hash.new()
+      @bids = Hash.new(-10000)
     end
 
     def place_bid(owner, price)
-      return :invaliv_bid if @bids[owner] > price or price <= self.min_prize
+      return :invalid_bid if @bids[owner] > price or price <= self.min_prize
       return :bid_already_made if @bids.values.detect {|bid| bid==price}
       @bids[owner] = price
+      update_current_winner
       return :success
+    end
+
+    def update_current_winner
+      temp_winner = nil
+      @bids.each{ |owner, price|  if @bids[temp_winner] < price
+                                    temp_winner = owner
+                                  end
+      }
+      @current_winner = temp_winner
     end
   end
 end
