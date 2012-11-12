f46e957e5d14d398d6e419c66b78804cb5c749db
shaechler <babemusicstar@hotmail.com>
2012-11-12 15:50:58 +0100

    added credit freeze

--- trade/app/models/module/auction.rb
+++ trade/app/models/module/auction.rb
@@ -12,6 +12,7 @@ module Models
     end
 
     def place_bid(owner, price)
+      return :not_enough_credits if owner.credits < price
       return :invalid_bid if @bids[owner] > price or price <= self.min_prize
       return :bid_already_made if @bids.values.detect {|bid| bid==price}
       @bids[owner] = price
@@ -21,11 +22,16 @@ module Models
 
     def update_current_winner
       temp_winner = nil
-      @bids.each{ |owner, price|  if @bids[temp_winner] < price
-                                    temp_winner = owner
-                                  end
+      @current_winner.credits += @bids[@current_winner] #SH Gives the money of the previous winner back
+
+      @bids.each{ |owner, price|
+        if @bids[temp_winner] < price
+          temp_winner = owner
+        end
       }
+
       @current_winner = temp_winner
+      @current_winner.credits -= @bids[@current_winner] #SH Deduct the money from the current winner
       @current_selling_price = @bids[@current_winner]-1 + increment
     end
   end
